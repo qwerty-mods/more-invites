72c65e998af6254b6a3b4990fc85b8f3fc61bb40
12944qwerty <qwerty12944qwerty@gmail.com>
2023-07-13 15:11:40 -0500

    lint

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import { Injector, Logger, webpack } from "replugged";
 const inject = new Injector();
 const logger = Logger.plugin("MoreInvites");
 
-const DSC_REGEX = /(?:https?:\/\/)?dsc\.gg\/(\w+?)(?:$|[^\w])/gm
+const DSC_REGEX = /(?:https?:\/\/)?dsc\.gg\/(\w+?)(?:$|[^\w])/gm;
 
 type CodedLinks = Array<{
   code?: string;
   type: string;
   from?: string; // for us
-}>
+}>;
 
 const cache: Record<string, string> = {};
 
@@ -24,9 +24,10 @@ function getCode(code: string): string {
     path: code,
     method: "GET",
     headers: {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.142.86 Safari/537.36",
-    }
-  }
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.142.86 Safari/537.36",
+    },
+  };
 
   request(options, (res) => {
     const { statusCode } = res;
@@ -37,7 +38,7 @@ function getCode(code: string): string {
     if (res.url) {
       cache[code] = res.url.split("/")[4];
     }
-  })
+  });
 
   return cache[code];
 }
@@ -49,18 +50,18 @@ export async function start(): Promise<void> {
   const key = webpack.getFunctionKeyBySource(mod, ".URL_REGEX)");
 
   if (mod && key) {
-    inject.after(mod, key, ([ args ], res) => {
+    inject.after(mod, key, ([args], res) => {
       console.log(args, res);
 
       if (args) {
         const matches = args.matchAll(DSC_REGEX);
         console.log(matches);
-        for (const [ , code ] of matches) {
+        for (const [, code] of matches) {
           res.push({
             code: getCode(code),
             type: "INVITE",
             from: "dsc.gg",
-          })
+          });
         }
       }
 
